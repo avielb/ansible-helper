ff2d33a693bb402753fed46b43bca8e2650451a5
Aviel Buskila <aviel.buskila@nielsen.com>
2017-02-08 16:49:24 +0200

    adding first version

---
+++ ansible-helper.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+if [ -z $WORKSPACE ]; then
+    WORKSPACE=`pwd`
+fi
+
+if [ -z $EDITOR ]; then
+    EDITOR="vim"
+    export EDITOR
+fi
+function create_role() {
+    echo "mkdir $WORKSPACE/roles/$1"
+    echo "mkdir $WORKSPACE/roles/$1/tasks"
+    echo "touch $WORKSPACE/roles/$1/tasks/main.yml"
+    echo "mkdir $WORKSPACE/roles/$1/handlers"
+    echo "touch $WORKSPACE/roles/$1/handlers/main.yml"
+    echo "mkdir $WORKSPACE/roles/$1/templates"
+    echo "mkdir $WORKSPACE/roles/$1/files"
+    echo "mkdir $WORKSPACE/roles/$1/vars"
+    echo "touch $WORKSPACE/roles/$1/vars/main.yml"
+    echo "mkdir $WORKSPACE/roles/$1/defaults"
+    echo "mkdir $WORKSPACE/roles/$1/vars"
+    echo "$EDITOR   $WORKSPACE/roles/$1/tasks/main.yml"
+}
+
+function delete_role(){
+    echo "rm -rf $WORKSPACE/roles/$1"
+}
+
+function list_roles(){
+    echo "ls $WORKSPACE/roles"
+}
+
+function edit_role(){
+    echo "$EDITOR $WORKSPACE/roles/$1/tasks/main.yml"
+}
+
+filename=default
+while (( $# > 0 ))
+do
+    opt="$1"
+    arg1="$2"
+    shift
+
+    case $opt in
+    --delete-role|-d)
+        if [[ $arg1 =~ ^[a-zA-Z_]+$ ]]; then
+            delete_role $arg1 
+        else
+            echo "ansible-helper $opt : Role name could be only small letters, capital letters and '_'"
+            exit 1
+        fi
+        exit 0
+        ;;
+    --edit-role|-e)
+        if [[ $arg1 =~ ^[a-zA-Z_]+$ ]]; then
+            edit_role $arg1 
+        else
+            echo "ansible-helper $opt : Role name could be only small letters, capital letters and '_'"
+            exit 1
+        fi
+        exit 0
+        ;;
+    --create-role|-c)
+        if [[ $arg1 =~ ^[a-zA-Z_]+$ ]]; then
+            create_role $arg1 
+        else
+            echo "ansible-helper $opt : Role name could be only small letters, capital letters and '_'"
+            exit 1
+        fi
+        exit 0
+        ;;
+    --list-role|-l)
+        list_roles
+        exit 0
+        ;;
+    --help)
+        helpfunc
+        exit 0
+        ;;
+    --version)
+        echo "$0 version 1.0.0"
+        exit 0
+        ;;
+    --file)  # Example with an operand
+        filename="$1"
+        shift
+        ;;
+    --*)
+        echo "Invalid option: '$opt'" >&2
+        exit 1
+        ;;
+    *)
+        # end of long options
+        break;
+        ;;
+   esac
+
+done
